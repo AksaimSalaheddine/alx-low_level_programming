2b8938d53b66b86bf47e5fc3cae42122a1fd15fb
Salaheddine aksaim <aksaimsalaheddine@gmail.com>
2023-03-16 17:12:03 -0700

    Tasks 0-5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char message[] = "\"Programming is like building a multilingual puzzles";
+
+	puts(message);
+	/** return */
+	return (0);
+}
