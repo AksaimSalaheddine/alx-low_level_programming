e058d8d853f7ec401b18c6a0fdf9ad77cf7160b8
Salaheddine aksaim <aksaimsalaheddine@gmail.com>
2023-03-17 13:31:53 -0700

    task 2-8

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: - return 0
+*/
+int main(void)
+{
+	char a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+	}
+	putchar('\n');
+	return (0);
+}
