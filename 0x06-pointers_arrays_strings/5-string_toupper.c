610066a969f249a7152e40f945d5de8404150e7d
Salaheddine aksaim <aksaimsalaheddine@gmail.com>
2023-03-30 16:39:32 -0700

    task5-6

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,9 +7,10 @@ char *string_toupper(char *s)
 {
 	int i;
 
-	for (i = 0; i >= 97 && i <= 122; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		s[i] -= 32;
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] -= 32;
 	}
 	return (s);
 
