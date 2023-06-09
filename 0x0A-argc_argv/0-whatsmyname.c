54debefc3f4e3ca0cfe6dfee0f31959ce994263f
AksaimSalaheddine <aksaimsalaheddine@gmail.com>
2023-06-09 14:31:50 -0700

    happy-betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- *
- * @ac: argemunt count
- * @av: argement victor
- *
- * Return: 0
- */
+*main - entry point
+*
+* @ac: argemunt count
+* @av: argement victor
+*
+* Return: 0
+*/
 
 int main(int ac, char **av)
 {
-  if (ac > 0)
-    {
-    printf("this is my name: %s\n", av[0]);
-    }
-  return (0);
+	if (ac > 0)
+	{
+		printf("this is my name: %s\n", av[0]);
+	}
+	return (0);
 }
