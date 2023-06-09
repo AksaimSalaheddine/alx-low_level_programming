30af4799473e4ceaece2162c826f6944c15e72c5
AksaimSalaheddine <aksaimsalaheddine@gmail.com>
2023-06-09 16:46:45 -0700

    fixed

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,12 +12,15 @@
 
 int main(int ac, char *av[])
 {
+	if (ac != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
 	int ar1 = atoi(av[1]);
 	int ar2 = atoi(av[2]);
 
-	if (ac != 3)
-		printf("Error\n");
-	else
-		printf("%i\n", ar1 * ar2);
+	printf("%i\n", ar1 * ar2);
 	return (0);
 }
