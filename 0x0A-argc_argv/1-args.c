683482b36a77c14b8da44072d064b0aa447620d7
AksaimSalaheddine <aksaimsalaheddine@gmail.com>
2023-06-09 16:04:15 -0700

    task-1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @ac: arg count
+ * @av: arg victor
+ *
+ * Return: 0
+ */
+
+int main(int ac, char *av[])
+{
+	printf("%d\n", ac);
+	return (0);
+}
