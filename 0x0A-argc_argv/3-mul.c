820fb41c2c841fd3c14f4c9ab2fc1c2b6099a30d
AksaimSalaheddine <aksaimsalaheddine@gmail.com>
2023-06-09 16:57:11 -0700

    test

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,23 +4,23 @@
 /**
  * main - entry point
  *
- * @ac: arg count
- * @av: arg victor
+ * @argc: int
+ * @argv: vector
  *
  * Return: 0
  */
 
-int main(int ac, char *av[])
+int main(int argc, char *argv[])
 {
-	if (ac != 3)
+	int arg1, arg2;
+
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	int ar1 = strtol(av[1], NULL, 10);
-	int ar2 = strtol(av[2], NULL, 10);
-
-	printf("%i\n", ar1 * ar2);
+	arg1 = strtol(argv[1], NULL, 10);
+	arg2 = strtol(argv[2], NULL, 10);
+	printf("%i\n", arg1 * arg2);
 	return (0);
 }
