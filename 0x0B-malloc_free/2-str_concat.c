2f80df4a500edc7c78103dcd65ffd56b29530b0f
AksaimSalaheddine <aksaimsalaheddine@gmail.com>
2023-06-12 18:17:08 -0700

    fixed the free issue

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,16 +16,17 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, len1, len2;
 	char *array_t;
 
-	for (len1 = 0; s1[len1] != '\0'; len1++)
+	for (len1 = 0; s1 != NULL && s1[len1] != '\0'; len1++)
 		;
-	for (len2 = 0; s2[len2] != '\0'; len2++)
+	for (len2 = 0; s2 != NULL && s2[len2] != '\0'; len2++)
 		;
 	array_t = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
 	if (array_t == NULL)
-		return(NULL);
+		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
 		array_t[i] = s1[i];
-	for (i =0; s2[i] != '\0'; i++)
+	for (i = 0; s2[i] != '\0'; i++)
 		array_t[len1 + i] = s2[i];
-	return(array_t);
+	return (array_t);
+	free(array_t);
 }
